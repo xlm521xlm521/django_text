281a28024469e8a26552731567e905440890f31a
xlm <1158529273@qq.com>
2016-03-08 22:09:52 +0800

    add_page

--- rango/urls.py
+++ rango/urls.py
@@ -5,5 +5,6 @@ urlpatterns = patterns( '',
         url(r'^$', views.index, name='index' ) ,
         url(r'^showpython/', views.showpython, name='showpython'),
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$', views.category, name='category'), # New!
-        url( r'^add_category/$' , views.add_category, name='add_category' ) , # NEW MAPPING!   
+        url(r'^add_category/$' , views.add_category, name='add_category' ) , # NEW MAPPING!   
+        url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$', views.add_page, name='add_page'),
 )
--- rango/views.py
+++ rango/views.py
@@ -19,6 +19,7 @@ def category(request, category_name_slug) :
 		pages = Page.objects.filter( category=category)
 		context_dict['pages' ] = pages
 		context_dict['category' ] = category
+		context_dict['category_name_slug' ] =category_name_slug #new
 	except Category.DoesNotExist:
 		pass
 	return render( request, 'rango/category.html' , context_dict)
@@ -34,3 +35,26 @@ def add_category( request) :
 	else:
 		form = CategoryForm( )
 	return render( request, 'rango/add_category.html' , {'form' : form})
+
+from rango.forms import PageForm
+def add_page(request, category_name_slug) :
+	try:
+		cat = Category.objects.get( slug=category_name_slug)
+	except Category.DoesNotExist:
+		cat = None
+	if request.method == 'POST' :
+		form = PageForm(request.POST)
+		if form.is_valid( ) :
+			if cat:
+				page = form.save( commit=False)
+				page.category = cat
+				page.views = 0
+				page.save( )
+				# probably better to use a redirect here.
+				return category( request, category_name_slug)
+			else:
+				print form.errors
+	else:
+		form = PageForm( )
+	context_dict = {'form' : form, 'category' : cat}
+	return render( request, 'rango/add_page.html' , context_dict)
